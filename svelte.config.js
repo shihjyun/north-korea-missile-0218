349dc16dd4ab93db16910ce5729adcc456932648
Steven Yeo <shihjyun.yeo@cw.com.tw>
2023-02-19 08:23:58 +0800

    fix build

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,23 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess'
-import seqPreprocessor from 'svelte-sequential-preprocessor'
+import preprocess from 'svelte-preprocess';
+import seqPreprocessor from 'svelte-sequential-preprocessor';
 import { preprocessThrelte } from '@threlte/preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
+			// default options are shown
 			pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false,
-      strict: false
-		})
+			assets: 'build',
+			fallback: null
+		}),
+		files: {
+			assets: 'static',
+			lib: 'src/components',
+			routes: 'src/routes',
+			appTemplate: 'src/app.html'
+		}
 	},
 	preprocess: seqPreprocessor([preprocess(), preprocessThrelte()])
 };
